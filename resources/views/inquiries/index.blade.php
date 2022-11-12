093a6e9df9838aa8c76051db732f744613dc1073
Hatim Lagzar <lhatimdev@gmail.com>
2022-11-12 08:53:01 +0100

    change inquiries to enquiries

--- resources/views/inquiries/index.blade.php
+++ resources/views/inquiries/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Inquiries</h1>
+    <h1 class="h2">Enquiries</h1>
 </div>
 
 <table class="table">
